4aad1d91c43bea734d89b9099e48121feb211186
ilovenature <ilovenature@ilovenature-no-MacBook.local>
2018-07-22 18:21:20 +0900

    new index.php overwrite

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
-echo "Hello, World!"
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . '/vendor/autoload.php';
+// POSTメソッドで渡される値を取得、表示
+$inputString = file_get_contents('php://input'); 
+error_log($inputString);
 
 ?>
